c7dc349d15b3c88f64c9ffd4e6c4dfd4281c2621
Tom <25082829+truedl@users.noreply.github.com>
2019-07-05 16:06:40 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='aiosteam',
+    version="0.0.1a",
+    author='truedl',
+    description='An asynchronous steam API wrapper written in Python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/truedl/aiosteam',
+    packages=['aiosteam'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
